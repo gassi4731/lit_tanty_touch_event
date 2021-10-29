6e85d6594eeb13426b6accd7be8816f09b61cb21
Yo Higashida <penguin4731@gmail.com>
2021-10-29 21:27:30 +0900

    現状をpush

--- touch event/ViewController.swift
+++ touch event/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController  , UIImagePickerController,
+                      UINavigationControllerDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let touch: UITouch = touches.first!
+        let location: CGPoint = touch.location(in: self.view)
+        print("タッチした位置（\(location.x) , \(location.y))")
+    }
 
-
+    var imagePickerController: UIImagePickerController =
+    UIImagePickerController()
+    
+    imagePickerController.sourceType =
+    UIImagePickerController.SourceType.PhotoLibrary
+    
+    imagePickerController.allowsEditing = true
+    
+    self.present(imagePickerController, animated:
+                    true, completion: nil)
+    
+    
+    imagePickerController.delegate = self
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediawithInfo into: [UIImagePickerController.InfoKey : Any]){
+        let image = info[.originalImage] as? UIImage
+        
+        haikeiImageView.image = image
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
+    
+    
+    
 }
 
